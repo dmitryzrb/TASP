c7ba95f22c32d732cf00ddeeed2ea2c3c286c666
taefNiki <taefniki001@mail.ru>
2018-01-22 21:54:46 +0300

    dfdfdfsadasdfaft

--- app/src/main/java/com/example/dmitr/testapp/SettingsDrugs.java
+++ app/src/main/java/com/example/dmitr/testapp/SettingsDrugs.java
@@ -1,7 +1,10 @@
 package com.example.dmitr.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsDrugs extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SettingsDrugs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_drugs);
+        Button ChangeNameButton = findViewById(R.id.buttonOK);
+        ChangeNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsDrugs.this, Buttons3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
